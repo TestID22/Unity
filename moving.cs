ff35d8981c96dcc0d199d08e7c3ff60eac16568f
vitaliiseptember@gmail.com <vitaliiseptember@gmail.com>
2019-05-16 09:53:25 +0300

    move

---
+++ moving.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float horizantalSpeed;
+    float speedX;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            speedX = - horizantalSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            speedX = horizantalSpeed;
+        }
+        transform.Translate(speedX, 0, 0);
+        speedX = 0;
+    }
+}
